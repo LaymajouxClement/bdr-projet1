29cc7867fded90014844da1fcc6289fa44cf8ffc
Louison-Diguer <71321509+Louison-Diguer@users.noreply.github.com>
2023-10-18 09:29:58 -0400

    users

---
+++ scripts/Site3.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE LINK Site3ToSystem
+CONNECT TO system IDENTIFIED BY admin
+USING 'localhost:1521/XE';
---
+++ scripts/Site4.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE LINK Site4ToSystem
+CONNECT TO system IDENTIFIED BY admin
+USING 'localhost:1521/XE';
